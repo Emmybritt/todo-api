fea75a83e7a4fd9cc27e94944848a25ab869dda1
Ogwu Emmanuel Berit <beritogwu@gmail.com>
2023-08-29 17:17:49 +0100

    remove cors

--- build/app.js
+++ build/app.js
@@ -4,21 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const routes_1 = require("./routes");
 const app = (0, express_1.default)();
-// app.use();
-app.use((0, cors_1.default)({
-    origin: [
-        '*',
-        // 'http://localhost:5173',
-        // 'https://sprightly-marzipan-3f83a5.netlify.app',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: false,
-}));
 app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,6 @@ import bodyParser from 'body-parser';
 import { router } from './routes';
 
 const app = express();
-// app.use();
-app.use(
-    cors({
-        origin: [
-            '*',
-            // 'http://localhost:5173',
-            // 'https://sprightly-marzipan-3f83a5.netlify.app',
-        ],
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: false,
-    }),
-);
 app.use(helmet());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
